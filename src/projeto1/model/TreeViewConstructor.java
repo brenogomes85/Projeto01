1c4cc8e9b8db43648f4a6e084d89ed069dacdfc3
brenogomes85 <brenogomes124@gmail.com>
2021-06-18 15:16:21 -0300

    Atualização Projeto 1

--- src/projeto1/model/TreeViewConstructor.java
+++ src/projeto1/model/TreeViewConstructor.java
@@ -11,7 +11,7 @@ public class TreeViewConstructor {
 	
 	//Este método cria um ArrayList de TreeItems (categorias)
 	@SuppressWarnings("unchecked")
-	public ArrayList<TreeItem> getCategorias(Linha valor){
+	public ArrayList<TreeItem> getCategorias(String linha){
 		
 		ArrayList<TreeItem> categorias = new ArrayList<TreeItem>();
 		
@@ -35,12 +35,12 @@ public class TreeViewConstructor {
 		aresTHS.getChildren().addAll(getAresTHS());
 		aresTHS.setExpanded(true);
 		
-		if(valor.getLinha()=="Cronos") {
+		if(linha=="Cronos") {
 			categorias.add(cronosOld);
 			categorias.add(cronosL);
 			categorias.add(cronosNG);
 		}
-		if(valor.getLinha()=="Ares") {
+		if(linha=="Ares") {
 			categorias.add(aresTB);
 			categorias.add(aresTHS);
 		}
@@ -54,9 +54,9 @@ public class TreeViewConstructor {
 		
 		ArrayList<TreeItem> cronosOld = new ArrayList<TreeItem>();
 		
-		TreeItem cronosOld1 = new TreeItem("Cronos 6001-A");
-		TreeItem cronosOld2 = new TreeItem("Cronos 6003");
-		TreeItem cronosOld3 = new TreeItem("Cronos 7023");
+		TreeItem cronosOld1 = new TreeItem(CategoriaMedidores.CRONOS_OLD1);
+		TreeItem cronosOld2 = new TreeItem(CategoriaMedidores.CRONOS_OLD2);
+		TreeItem cronosOld3 = new TreeItem(CategoriaMedidores.CRONOS_OLD3);
 		
 		cronosOld.add(cronosOld1);
 		cronosOld.add(cronosOld2);
@@ -71,8 +71,8 @@ public class TreeViewConstructor {
 		
 		ArrayList<TreeItem> cronosL = new ArrayList<TreeItem>();
 		
-		TreeItem cronosL1 = new TreeItem("Cronos 6021L");
-		TreeItem cronosL2 = new TreeItem("Cronos 7023L");
+		TreeItem cronosL1 = new TreeItem(CategoriaMedidores.CRONOS_L1);
+		TreeItem cronosL2 = new TreeItem(CategoriaMedidores.CRONOS_L2);
 		
 		cronosL.add(cronosL1);
 		cronosL.add(cronosL2);
@@ -86,12 +86,12 @@ public class TreeViewConstructor {
 		
 		ArrayList<TreeItem> cronosNG = new ArrayList<TreeItem>();
 		
-		TreeItem cronosNG1 = new TreeItem("Cronos 6001-NG");
-		TreeItem cronosNG2 = new TreeItem("Cronos 6003-NG");
-		TreeItem cronosNG3 = new TreeItem("Cronos 6021-NG");
-		TreeItem cronosNG4 = new TreeItem("Cronos 6031-NG");
-		TreeItem cronosNG5 = new TreeItem("Cronos 7021-NG");
-		TreeItem cronosNG6 = new TreeItem("Cronos 7023-NG");
+		TreeItem cronosNG1 = new TreeItem(CategoriaMedidores.CRONOS_NG1);
+		TreeItem cronosNG2 = new TreeItem(CategoriaMedidores.CRONOS_NG2);
+		TreeItem cronosNG3 = new TreeItem(CategoriaMedidores.CRONOS_NG3);
+		TreeItem cronosNG4 = new TreeItem(CategoriaMedidores.CRONOS_NG4);
+		TreeItem cronosNG5 = new TreeItem(CategoriaMedidores.CRONOS_NG5);
+		TreeItem cronosNG6 = new TreeItem(CategoriaMedidores.CRONOS_NG6);
 		
 		cronosNG.add(cronosNG1);
 		cronosNG.add(cronosNG2);
@@ -109,9 +109,9 @@ public class TreeViewConstructor {
 		
 		ArrayList<TreeItem> aresTB = new ArrayList<TreeItem>();
 		
-		TreeItem aresTB1 = new TreeItem("Ares 7021");
-		TreeItem aresTB2 = new TreeItem("Ares 7031");
-		TreeItem aresTB3 = new TreeItem("Ares 7023");
+		TreeItem aresTB1 = new TreeItem(CategoriaMedidores.ARES_TB1);
+		TreeItem aresTB2 = new TreeItem(CategoriaMedidores.ARES_TB2);
+		TreeItem aresTB3 = new TreeItem(CategoriaMedidores.ARES_TB3);
 		
 		aresTB.add(aresTB1);
 		aresTB.add(aresTB2);
@@ -126,9 +126,9 @@ public class TreeViewConstructor {
 		
 		ArrayList<TreeItem> aresTHS = new ArrayList<TreeItem>();
 		
-		TreeItem aresTHS1 = new TreeItem("Ares 8023 15");
-		TreeItem aresTHS2 = new TreeItem("Ares 8023 200");
-		TreeItem aresTHS3 = new TreeItem("Ares 8023 2,5");
+		TreeItem aresTHS1 = new TreeItem(CategoriaMedidores.ARES_THS1);
+		TreeItem aresTHS2 = new TreeItem(CategoriaMedidores.ARES_THS2);
+		TreeItem aresTHS3 = new TreeItem(CategoriaMedidores.ARES_THS3);
 		
 		aresTHS.add(aresTHS1);
 		aresTHS.add(aresTHS2);
--- src/projeto1/view/WindowViewController.java
+++ src/projeto1/view/WindowViewController.java
@@ -2,9 +2,9 @@ package projeto1.view;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import projeto1.MainApp;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -14,17 +14,18 @@ import javafx.scene.control.TitledPane;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.control.Accordion;
-import javafx.scene.control.Button;
 import projeto1.model.TreeViewConstructor;
-import projeto1.model.Linha;
+import projeto1.model.CategoriaMedidores;
+import projeto1.model.LinhaEnum;
+import projeto1.model.ModelosMedidores;
 
 public class WindowViewController {
 	
-	private List<Linha> linha = new ArrayList<>();
+	private List<String> linha = new ArrayList<>();
 	private List<TreeViewConstructor> modelos = new ArrayList<>();
 	private List<TreeViewConstructor> categorias = new ArrayList<>();
 	
-	private ObservableList<Linha> obsLinha;
+	private ObservableList<String> obsLinha;
 	private ObservableList<TreeViewConstructor> obsModelos;
 	private ObservableList<TreeViewConstructor> obsCategorias;
 	
@@ -35,7 +36,7 @@ public class WindowViewController {
     private TitledPane tpLinha;
 
     @FXML
-    private ComboBox<Linha> comboBoxLinhas;
+    private ComboBox<String> comboBoxLinhas;
 
     @FXML
     private TitledPane modelo;
@@ -58,11 +59,11 @@ public class WindowViewController {
     //Insere/Carrega as linhas de modelo (Cronos, Ares) no combobox 
     public void carregaLinhas() {
     	
-    	Linha linha1 = new Linha("Cronos", 1);
-    	Linha linha2 = new Linha("Ares", 2);
+    	LinhaEnum linha1 = LinhaEnum.CRONOS;
+    	LinhaEnum linha2 = LinhaEnum.ARES;
     	
-    	linha.add(linha1);
-    	linha.add(linha2);
+    	linha.add(linha1.getLinha());
+    	linha.add(linha2.getLinha());
     	
     	obsLinha = FXCollections.observableArrayList(linha);
     	
@@ -70,29 +71,51 @@ public class WindowViewController {
     }
     
     /*
-     * Carrega os dados na tree view
+    * Carrega os dados na tree view
     */
-    public TreeView<String> carregaTreeView() {
-    	//Cria a TreeView constructor
-    	TreeViewConstructor constructor = new TreeViewConstructor();
-    	
-    	/*
+    @SuppressWarnings("unchecked")
+    public TreeView<String> carregaTreeView(){
+    	 /* 
     	 * Verifica o valor selecionado no combobox e constroi a tree view
     	 * de acordo com a opção escolhida.
     	 */
-    	comboBoxLinhas.getSelectionModel().selectedItemProperty().addListener((options, oldValue, newValue) -> {
-    		
+    	comboBoxLinhas.getSelectionModel().selectedItemProperty().addListener((options, oldValue, newValue) ->{
     		//Recebe o valor selecionado no combobox
-    		Linha linha = comboBoxLinhas.getValue();
+    		String linha = comboBoxLinhas.getValue();
+    		//TreeViewConstructor aux = new TreeViewConstructor();
     		
-    		//Get the categorias
-        	ArrayList<TreeItem> categorias = constructor.getCategorias(linha);
-        	
-        	//Cria a raiz da Tree View
-        	TreeItem raiz = new TreeItem("");
-        	
-        	//Adiciona filhas na raiz
-        	raiz.getChildren().addAll(categorias);
+    		
+    		//Cria a raiz da Tree View
+    		TreeItem raiz = new TreeItem("");
+    		
+    		ArrayList <TreeItem> categoria = new ArrayList<TreeItem>();
+    		
+    		switch(linha){
+    			case "Cronos":
+    				TreeItem cronosOld = new TreeItem<>(ModelosMedidores.CRONOS_OLD.getModelo());
+	    			cronosOld.getChildren().add(new TreeItem<>(CategoriaMedidores.CRONOS_OLD1.getCategoria()));
+	    			cronosOld.getChildren().add(new TreeItem<>(CategoriaMedidores.CRONOS_OLD2.getCategoria()));
+	    			cronosOld.getChildren().add(new TreeItem<>(CategoriaMedidores.CRONOS_OLD3.getCategoria()));
+	    		
+	    			/*TreeItem cronosL = new TreeItem(ModelosMedidores.CRONOS_L);
+	    			cronosL.getChildren().addAll(CategoriaMedidores.CRONOS_OLD1, CategoriaMedidores.CRONOS_OLD2,
+	    											CategoriaMedidores.CRONOS_OLD3);
+	    			
+	    			TreeItem cronosNG = new TreeItem(ModelosMedidores.CRONOS_NG);
+	    			cronosNG.getChildren().addAll(CategoriaMedidores.CRONOS_NG1, CategoriaMedidores.CRONOS_NG2,
+	    											CategoriaMedidores.CRONOS_NG3, CategoriaMedidores.CRONOS_NG4,
+	    											CategoriaMedidores.CRONOS_NG5, CategoriaMedidores.CRONOS_NG6);*/
+	    			cronosOld.setExpanded(true);
+	    			//cronosL.setExpanded(true);
+	    			//cronosNG.setExpanded(true);
+	    			
+	    			categoria.add(cronosOld);
+	    			//categoria.add(cronosL);
+	    			//categoria.add(cronosNG);
+    		}
+    		
+    		//Adiciona filhas na raiz
+        	raiz.getChildren().addAll(categoria);
         	
         	//Seta o nó da raiz
         	treeViewModelos.setRoot(raiz);
@@ -102,10 +125,10 @@ public class WindowViewController {
         	
         	//Ativa o titledPane após carregar a tree view
         	modelo.setDisable(false);
+    		
     	});
     	
-    	//Retorna a Tree View de acordo com a seleção feita no ComboBox.
     	return treeViewModelos;
-    		
-    }
+    	
+    }   
 }
